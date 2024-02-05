09e7905d7223d2fa4535bb54de8107d2c2d5ba10
carloodq <dallaquerciacarlo@gmail.com>
2024-02-05 16:36:02 +0100

    Molecules can only contain CNOPSH - fixiing column for original xyz - using all conformers from DES15K

--- bde_data/generate_csv.py
+++ bde_data/generate_csv.py
@@ -3,7 +3,7 @@ from extract_xyz import extract_xyz
 import os
 from rdkit import Chem
 from rdkit.Chem import rdDetermineBonds
-df = pd.read_csv("Donchev et al DES370K.csv")
+# df = pd.read_csv("Donchev et al DES370K.csv")
 # extract_xyz(df.head(100))
 
 def create_newxyzs(m, item):
@@ -12,23 +12,26 @@ def create_newxyzs(m, item):
     takes as input a Mol RDkit object of a molecule, returns a list of broken xyzs with respective parameters
     '''
     newxyzs = []
-    for atom in m.GetAtoms():
-        if atom.GetSymbol() == 'H':
-            h = atom.GetIdx()
-            fragment = str(atom.GetOwningMol())
-            ns = atom.GetNeighbors()
-            if len(ns) > 1: # if not outside
-                continue
-            else:
-                for n in ns:
-                    if n.GetSymbol() in 'CNOPS':
-                        organic = n.GetIdx()
-                        neighbors =  [n.GetSymbol() for n in n.GetNeighbors()] 
-                        # removing atom to create mol 
-                        mw = Chem.RWMol(m)
-                        mw.RemoveAtom(h)
-                        xyz = Chem.MolToXYZBlock(mw)
-                        newxyzs.append([item , xyz , h , n.GetSymbol() , neighbors, fragment])
+    # checking if dimer isonly made f CNOPS and H
+    if len(m.GetAtoms()) == len([a.GetSymbol() for a in m.GetAtoms() if a.GetSymbol() in 'CNOPSH' ]):
+        for atom in m.GetAtoms():
+            if atom.GetSymbol() == 'H':
+                h = atom.GetIdx()
+                fragment = str(atom.GetOwningMol())
+                ns = atom.GetNeighbors()
+                if len(ns) > 1: # if not outside
+                    continue
+                else:
+                    for n in ns:
+                        if n.GetSymbol() in 'CNOPS':
+                            organic = n.GetIdx()
+                            neighbors =  [n.GetSymbol() for n in n.GetNeighbors()] 
+                            # removing atom to create mol 
+                            mw = Chem.RWMol(m)
+                            mw.RemoveAtom(h)
+                            xyz_reduced = Chem.MolToXYZBlock(mw)
+                            xyz = Chem.MolToXYZBlock(m)
+                            newxyzs.append([item , xyz , xyz_reduced,  h , n.GetSymbol() , neighbors, fragment])
     return newxyzs
 
 cwd = os.getcwd()
@@ -41,15 +44,17 @@ for item in items:
         # path to the xyz file
         xyzf = os.path.join(cwd, item, 'qm_opt_dimer', all_xyzs[0])
         xyz = os.listdir(xyzf)
-        xyzp = os.path.join(cwd, item, 'qm_opt_dimer', all_xyzs[0], xyz[0])
-        # open the xyz and add the bonds
-        raw_mol = Chem.MolFromXYZFile(xyzp)
-        mol = Chem.Mol(raw_mol)
-        rdDetermineBonds.DetermineConnectivity(mol)
-        m = mol
-        newxyzs = create_newxyzs(m, item)  
-        all_xyz.extend(newxyzs)   
+        # add all the conformers for each dimer 
+        for conformer in xyz:
+            xyzp = os.path.join(cwd, item, 'qm_opt_dimer', all_xyzs[0], conformer)
+            # open the xyz and add the bonds
+            raw_mol = Chem.MolFromXYZFile(xyzp)
+            mol = Chem.Mol(raw_mol)
+            rdDetermineBonds.DetermineConnectivity(mol)
+            m = mol
+            newxyzs = create_newxyzs(m, item)  
+            all_xyz.extend(newxyzs)   
 
-all_xyz = pd.DataFrame(all_xyz, columns=['molecule_name', 'xyz', 'atom_index', 'attached_atom', 'neighboring_atoms', 'fragment'])
+all_xyz = pd.DataFrame(all_xyz, columns=['molecule_name', 'xyz', 'xyz_reduced', 'atom_index', 'attached_atom', 'neighboring_atoms', 'fragment'])
 # save it 
 all_xyz.to_csv('all_xyz.csv')
